b3adef094b2296edb1391394c3498f66789324c9
Lucas Rijllart <lucasrijllart@Lucas-M2-Air.local>
2023-04-20 17:33:21 -0500

    dummy file and test

---
+++ aoc/d1/main_test.py
@@ -0,0 +1,14 @@
+"""Tests for main module."""
+import unittest
+
+from . import main
+
+
+class TestDay1(unittest.TestCase):
+
+    def test_dummy(self):
+        """Dummy test"""
+        assert main.dummy() is True
+
+if __name__ == '__main__':
+    unittest.main()
