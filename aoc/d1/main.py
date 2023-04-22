daf98c274f07bb1a63aab6c9f2a1950551b6e2dc
Lucas Rijllart <11093148+lucasrijllart@users.noreply.github.com>
2023-04-21 20:44:25 -0500

    solution to day 1

--- aoc/d1/main.py
+++ aoc/d1/main.py
@@ -1,2 +1,19 @@
-def dummy():
-    return True
+"""Day 1"""
+
+def part_1():
+    file = open("input.txt", "r")
+    data = [int(line.strip()) for line in file.readlines()]
+    result = get_increasing_measurements(data)
+    print(result)
+
+def get_increasing_measurements(measurements):
+    """Return the number of measurements that are bigger than the previous."""
+    increasing_measurements = 0
+    for index, measure in enumerate(measurements):
+        if index == 0:
+            continue
+        if measure > measurements[index-1]:
+            increasing_measurements += 1
+    return increasing_measurements
+
+part_1()
--- aoc/d1/test_main.py
+++ aoc/d1/test_main.py
@@ -6,9 +6,12 @@ from . import main
 
 class TestDay1(unittest.TestCase):
 
-    def test_dummy(self):
-        """Dummy test"""
-        assert main.dummy() is True
+    def test_get_increasing_measurements(self):
+        """Test for part 1"""
+        inputs = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        result = main.get_increasing_measurements(inputs)
+        self.assertEqual(result, 7)
+        
 
 if __name__ == '__main__':
     unittest.main()
