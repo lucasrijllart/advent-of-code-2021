3ac60d017ac1adea7314495541993888d6c43161
Lucas Rijllart <11093148+lucasrijllart@users.noreply.github.com>
2023-04-24 17:47:39 -0500

    Day 1 Part 1 (#2)
    * solution to day 1
    
    * added test
    
    * add file path in tests
    
    * closing file

--- aoc/d1/main.py
+++ aoc/d1/main.py
@@ -1,2 +1,24 @@
-def dummy():
-    return True
+"""Day 1"""
+
+def part_1(file_name):
+    """Return the number of measurements that are in increasing order."""
+    import os
+    a= os.listdir('.')
+    with open(file_name, "r") as file:
+        data = [int(line.strip()) for line in file.readlines()]  # parsing
+    return get_increasing_measurements(data)
+
+
+def get_increasing_measurements(measurements):
+    """Return the number of measurements that are bigger than the previous."""
+    increasing_measurements = 0
+    for index, measure in enumerate(measurements):
+        if index == 0:
+            continue
+        if measure > measurements[index-1]:
+            increasing_measurements += 1
+    return increasing_measurements
+
+if __name__ == "__main__":
+    part_1_result = part_1("input.txt")
+    print("Part 1:", part_1_result)
