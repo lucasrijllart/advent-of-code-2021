65803e100972ddc79529bd719fc6653c688a5b47
Lucas Rijllart <11093148+lucasrijllart@users.noreply.github.com>
2023-04-24 17:38:55 -0500

    added test

--- aoc/d1/main.py
+++ aoc/d1/main.py
@@ -1,10 +1,11 @@
 """Day 1"""
 
-def part_1():
-    file = open("input.txt", "r")
-    data = [int(line.strip()) for line in file.readlines()]
-    result = get_increasing_measurements(data)
-    print(result)
+def part_1(file_name):
+    """Return the number of measurements that are in increasing order."""
+    file = open(file_name, "r")
+    data = [int(line.strip()) for line in file.readlines()]  # parsing
+    return get_increasing_measurements(data)
+
 
 def get_increasing_measurements(measurements):
     """Return the number of measurements that are bigger than the previous."""
@@ -16,4 +17,5 @@ def get_increasing_measurements(measurements):
             increasing_measurements += 1
     return increasing_measurements
 
-part_1()
+part_1_result = part_1("input.txt")
+print("Part 1:", part_1_result)
--- aoc/d1/test_main.py
+++ aoc/d1/test_main.py
@@ -7,10 +7,16 @@ from . import main
 class TestDay1(unittest.TestCase):
 
     def test_get_increasing_measurements(self):
-        """Test for part 1"""
+        """Ensure the function returns the correct number of measurements that are in increasing order."""
         inputs = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
         result = main.get_increasing_measurements(inputs)
         self.assertEqual(result, 7)
+
+    def test_part_1(self):
+        """Test for part 1 with example file."""
+        expected_result = 1226
+        result = main.part_1("test.txt")
+        self.assertEqual(result, expected_result)
         
 
 if __name__ == '__main__':
