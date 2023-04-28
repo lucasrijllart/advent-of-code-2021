84587d258c40353a8e88a55bde4cbe856309e0fd
Lucas Rijllart <11093148+lucasrijllart@users.noreply.github.com>
2023-04-28 17:34:38 -0500

    Day 1 Part 2 (#3)

--- aoc/d1/main.py
+++ aoc/d1/main.py
@@ -2,12 +2,19 @@
 
 def part_1(file_name):
     """Return the number of measurements that are in increasing order."""
-    import os
-    a= os.listdir('.')
-    with open(file_name, "r") as file:
-        data = [int(line.strip()) for line in file.readlines()]  # parsing
+    data = parse_input(file_name)
     return get_increasing_measurements(data)
 
+def part_2(file_name):
+    """Return the number of measurements that are in increasing order."""
+    data = parse_input(file_name)
+    return get_increasing_sliding_window(data)
+
+def parse_input(file_name: str) -> list:
+    """Return a integer list for a given file name. Format must conform to example given."""
+    with open(file_name, "r") as file:
+        data = [int(line.strip()) for line in file.readlines()]  # parsing
+    return data
 
 def get_increasing_measurements(measurements):
     """Return the number of measurements that are bigger than the previous."""
@@ -19,6 +26,20 @@ def get_increasing_measurements(measurements):
             increasing_measurements += 1
     return increasing_measurements
 
+def get_increasing_sliding_window(measurements):
+    """Return the number of measurements that are bigger than the previous in a
+    three-measurement sliding window."""
+    increasing_measurements = 0
+    for index, measure in enumerate(measurements[:-3]):
+        first_window = measure + measurements[index+1] + measurements[index+2]
+        second_window = measurements[index+1] + measurements[index+2] + measurements[index+3]
+        if first_window < second_window:
+            increasing_measurements += 1
+    return increasing_measurements
+
 if __name__ == "__main__":
-    part_1_result = part_1("input.txt")
+    file_name = "input.txt"
+    part_1_result = part_1(file_name)
     print("Part 1:", part_1_result)
+    part_2_result = part_2(file_name)
+    print("Part 2:", part_2_result)
--- aoc/d1/test_main.py
+++ aoc/d1/test_main.py
@@ -6,17 +6,36 @@ from . import main
 
 class TestDay1(unittest.TestCase):
 
-    def test_get_increasing_measurements(self):
-        """Ensure the function returns the correct number of measurements that are in increasing order."""
-        inputs = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-        result = main.get_increasing_measurements(inputs)
-        self.assertEqual(result, 7)
+    inputs = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    test_file_path = "aoc/d1/test.txt"
 
     def test_part_1(self):
         """Test for part 1 with example file."""
-        expected_result = 7
-        result = main.part_1("aoc/d1/test.txt")
-        self.assertEqual(result, expected_result)
+        result = main.part_1(self.test_file_path)
+        self.assertEqual(result, 7)
+
+    def test_part_2(self):
+        """Test for part 1 with example file."""
+        result = main.part_2(self.test_file_path)
+        self.assertEqual(result, 5)
+
+    def test_parse_input(self):
+        """Ensure the function returns the integer list representation of a test file."""
+        expected_output = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        data = main.parse_input(self.test_file_path)
+        self.assertEqual(data, expected_output)
+
+    def test_get_increasing_measurements(self):
+        """Ensure the function returns the correct number of measurements that are in increasing order."""
+        result = main.get_increasing_measurements(self.inputs)
+        self.assertEqual(result, 7)
+
+    def test_get_increasing_sliding_window(self):
+        """Ensure the function returns the correct number of measurements that are increasing in the sliding window."""
+        result = main.get_increasing_sliding_window(self.inputs)
+        self.assertEqual(result, 5)
+
+
         
 
 if __name__ == '__main__':
