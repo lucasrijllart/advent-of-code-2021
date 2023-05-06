5b608692424cc51d75e23feb81683b2bc3246108
Lucas Rijllart <11093148+lucasrijllart@users.noreply.github.com>
2023-05-05 22:10:12 -0500

    Day 2 (#4)
    * p2: add sliding window
    
    * Day 2 part 1
    
    * Day 2 Part 2

---
+++ aoc/d2/main.py
@@ -0,0 +1,64 @@
+"""Day 2"""
+
+def part_1(file_name):
+    """Return the distance and depth multiplied by each other."""
+    data = parse_input(file_name)
+    distance, depth = get_distance_and_depth(data)
+    return distance * depth
+
+def part_2(file_name):
+    """Not used yet"""
+    data = parse_input(file_name)
+    distance, depth = get_distance_and_depth_with_aim(data)
+    return distance * depth
+
+def parse_input(file_name: str) -> list:
+    """Return a list of tuples with the first character of the direction and the value.
+    Example: forwards 5 -> [("f", 5)]
+    """
+    data = []
+    with open(file_name, "r") as file:
+        for line in file.readlines():
+            line = line.strip()
+            data.append((line[0], int(line[-1])))
+    return data
+
+def get_distance_and_depth(data):
+    """Return two values: distance and depth. Distance is affected by forwards movement.
+    Depth is affected by up and down movement.
+    """
+    distance, depth = 0, 0
+    for entry in data:
+        direction, value = entry[0], entry[1]
+        if direction == "f":  # forwards
+            distance += value
+        else:
+            if direction == "d":  # down
+                depth += value
+            elif direction == "u":  # up
+                depth -= value
+    return distance, depth
+
+def get_distance_and_depth_with_aim(data):
+    """Return two values: distance and depth. Distance is affected by forwards movement.
+    Depth is affected by up and down movement.
+    """
+    distance, depth, aim = 0, 0, 0
+    for entry in data:
+        direction, value = entry[0], entry[1]
+        if direction == "f":  # forwards
+            distance += value
+            depth += aim * value
+        else:
+            if direction == "d":  # down
+                aim += value
+            elif direction == "u":  # up
+                aim -= value
+    return distance, depth
+
+if __name__ == "__main__":
+    file_name = "input.txt"
+    part_1_result = part_1(file_name)
+    print("Part 1:", part_1_result)
+    part_2_result = part_2(file_name)
+    print("Part 2:", part_2_result)
---
+++ aoc/d2/test_main.py
@@ -0,0 +1,35 @@
+"""Tests for main module."""
+import unittest
+
+from . import main
+
+
+class TestDay2(unittest.TestCase):
+
+    inputs = []
+    test_file_path = "aoc/d2/test.txt"
+
+    def test_part_1(self):
+        """Test for part 1 with example file."""
+        result = main.part_1(self.test_file_path)
+        self.assertEqual(result, 150)
+
+    def test_part_2(self):
+        """Test for part 1 with example file."""
+
+    def test_parse_input(self):
+        """Ensure the function returns the list of tuples that represents the input file."""
+        expected_output = [("f", 5), ("d", 5), ("f", 8), ("u", 3), ("d", 8), ("f", 2)]
+        data = main.parse_input(self.test_file_path)
+        self.assertEqual(data, expected_output)
+
+    def test_get_distance_and_depth_with_aim(self):
+        input_data = [("f", 5), ("d", 5), ("f", 8), ("u", 3), ("d", 8), ("f", 2)]
+        distance, depth = main.get_distance_and_depth_with_aim(input_data)
+        self.assertEqual(distance, 15)
+        self.assertEqual(depth, 60)
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
